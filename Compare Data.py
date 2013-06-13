fe37a106c53bc98af257922f1206af11551333aa
gneils <gneils@hotmail.com>
2013-06-13 12:30:48 -0700

    Automatically include folks born after certain date and in certain zipcode.

--- Compare Data.py
+++ Compare Data.py
@@ -81,7 +81,7 @@ for rowInSIR in sirDict:
 
 # SPSS IMPORT.   A SINGLE SPACE MEANS NOTHING WAS IN THE COLUMN
 minrow = 0
-maxrow = 20000
+maxrow = 10000
 makeDict (FromSPSS, SPSSDict)
 
 for rowInSPSS in SPSSDict:
@@ -92,10 +92,12 @@ for rowInSPSS in SPSSDict:
     rowInSPSS['dob'] = rowInSPSS['dobmm'] + '/' + rowInSPSS['dobdd'] + '/' + rowInSPSS['dobccyy']
     rowInSPSS['address1'] += ' ' + rowInSPSS['address2'] 
     rowInSPSS['address1'] = AC.cleanAddress(rowInSPSS['address1'] )
+    rowInSPSS['dataSource'] = 'na'
     rowInSPSS['highPoint'] = 0
     rowInSPSS['highPhone']   = -1
     rowInSPSS['highFirstName']   = -1
     rowInSPSS['highLastName']   = -1
+    rowInSPSS['highMixedName']   = -1
     rowInSPSS['highAddress'] = -1
     rowInSPSS['highSex']     = -1
     rowInSPSS['multFactor']  = -1
@@ -158,6 +160,8 @@ def compareAddress (a1, a2):
     points = 0
     if a1 == '' or a2 == '':
         return (points)
+    if a1 == a2
+        points += 10
     if a2 in a1 or a1 in a2:
         points += 8
     for aword in a2.split():
@@ -165,24 +169,14 @@ def compareAddress (a1, a2):
             points += 1
     return (points)
 
-def compareLName (lname1, lname2):
+def compareName (name1, name2):
     points = 0
-    if lname1 == lname2:
+    if name1 == name2:
         points = 10
-    elif lname1 in lname2 or lname2 in lname1:
-        points = 7
-        
+    elif name1 in name2 or name2 in name1:
+        points = 7        
     return (points)
     
-def compareFName (fname1, fname2):
-    points = 0
-    if fname1 == fname2:
-        points = 10
-    elif fname1 in fname2 or fname2 in fname1:
-        points = 6
-    return (points)
-
-
 def comparePhone (phone1, phone2):
     points = 0
     if phone1 == '' or phone2 == '':
@@ -199,6 +193,31 @@ def compareSex (sex1, sex2):
         points = 18
     return (points)
 
+def checkRow (lnameA, fnameA, lnameB, fnameB):
+    includeRow = 0
+    if (len(lnameA) == 0 or len(lnameB) == 0 ): return (includeRow)
+    if lnameA[0] ==  lnameB[0]: includeRow = 1
+    if lnameA[0] ==  fnameB[0]: includeRow = 1
+    if fnameA[0] ==  lnameB[0]: includeRow = 1
+    if fnameA[0] ==  fnameB[0]: includeRow = 1
+    return(includeRow)
+
+def afterJuly1943 (somedate):
+    # return True to skip the checks and automatically 
+    if somedate == '': return (False)
+    cutOff = time.strptime('07/01/1943', '%m/%d/%Y')
+    checkDate = time.strptime(somedate, '%m/%d/%Y')
+    if checkDate > cutOff: return (True)
+    return (False)
+
+def writeCSVrow ():
+    a.writerow(rowInSPSS)
+    highPointList.append(rowInSPSS['highPoint'])
+    print (SPSSLine , rowInSPSS['dataSource'], rowInSPSS['highPoint'], rowInSPSS['firstname'], rowInSPSS['lastname'], sep=',')
+
+
+
+
 ##sys.exit('Debugging address')
 
 print ('----Cycle Through Both Dictionaries----------')
@@ -206,27 +225,31 @@ SPSSLine = 0
 accessPhoneBook = ['subject_phone_home', 'subject_phone_mobile', 'subject_phone_work', 'subject_phone_nursing']
 SPSSPhoneBook = ['phonenumber']
 SIRPhoneBook = ['swphone', 'shphone', 'cwphone', 'chphone']
-outputFieldNames = ['highPoint', 'highFirstName', 'highLastName', 'highDOB', 'highSex', 'highAddress', 'highPhone', 'multFactor',
-                    'lastname', 'firstname', 'address1', 'dob' , 'gendercode',
+outputFieldNames = ['id', 'highPoint', 'dataSource', 'multFactor', 'highFirstName', 'highLastName', 'highFirstLast', 'highLastFirst',
+                    'highDOB', 'highSex', 'highAddress', 'highPhone', 
+                    'lastname', 'firstname', 'address1', 'dob' , 'gendercode', 'zip5',
                     'matchID', 'matchLname', 'matchFname' ,'matchDOB' , 'matchSex', 'matchAddress', 'matchPhone']
 
 with open(dups, 'w', newline='') as CSVDupObject:
-    print ('File Object Type: ' , type(CSVDupObject))
 
 ##    a = csv.DictWriter(CSVDupObject, delimiter=',', fieldnames=SPSSDict[0])
     a = csv.DictWriter(CSVDupObject, delimiter=',', fieldnames= outputFieldNames, extrasaction='ignore')
     a.writeheader()
 
-
-    
     for rowInSPSS in SPSSDict:
         SPSSLine += 1
-        sirLine = 0
+        if rowInSPSS['zip5'] == '10025':  # any person with a ZIP of 10025 will not be previously known to us
+            writeCSVrow ()
+            continue
+        if afterJuly1943(rowInSPSS['dob']):
+            writeCSVrow ()
+            continue
+
         SPSSPhoneList = []
         for eachSPSSPhone in SPSSPhoneBook:
             SPSSPhoneList.append(rowInSPSS[eachSPSSPhone])
         SPSSPhoneList = sorted(set(SPSSPhoneList))
-
+##      if Escape is press, stop for loop and display summary report.  Only works when py file is double clicked
         if msvcrt.kbhit() and msvcrt.getch() == chr(27).encode():
             aborted = True
             if aborted:
@@ -234,29 +257,29 @@ with open(dups, 'w', newline='') as CSVDupObject:
                 time.sleep(2)
                 break
 
-
+##      Compare to SIR data
         for rowInSIR in sirDict:
-            sirLine +=1
-            skipRow = 0
-            pointTotal = pointLastName = pointFirstName = pointDOB = pointSex = pointAddress = pointPhone = 0
+##          reset values
+            pointTotal = pointLastName = pointFirstName = pointFirstLast = pointLastFirst = 0
+            pointDOB = pointSex = pointAddress = pointPhone = 0
             matchingPhones = ''
             multFactor = 1
-    ##      speed things ups
-            includeRow = 1
-            if rowInSPSS['lastname'][0] ==  rowInSIR['fname'][0]: includeRow = 1
-            if rowInSPSS['lastname'][0] ==  rowInSIR['lname'][0]: includeRow = 1
-            if rowInSPSS['firstname'][0] ==  rowInSIR['lname'][0]: includeRow = 1
-            if rowInSPSS['firstname'][0] ==  rowInSIR['fname'][0]: includeRow = 1
 
+    ##      speed things ups, match only if first letter of either name match
+            includeRow = checkRow(rowInSPSS['lastname'], rowInSPSS['firstname'], rowInSIR['lname'],rowInSIR['fname'])
             if includeRow == 0:  continue               
                 
             pointAddress = compareAddress (rowInSPSS['address1'], rowInSIR['address'])
 
             pointDOB = compareDOB (rowInSPSS['dob'] , rowInSIR['db_dob'])
 
-            pointLastName = compareLName(rowInSPSS['lastname'], rowInSIR['lname'])
+            pointLastName = compareName(rowInSPSS['lastname'], rowInSIR['lname'])
 
-            pointFirstName = compareFName(rowInSPSS['firstname'], rowInSIR['fname'])
+            pointFirstName = compareName(rowInSPSS['firstname'], rowInSIR['fname'])
+
+            pointFirstLast = compareName(rowInSPSS['lastname'], rowInSIR['fname'])
+
+            pointLastFirst = compareName(rowInSPSS['firstname'], rowInSIR['lname'])
 
             pointSex = compareSex(rowInSPSS['gendercode'], rowInSIR['db_sex'])
             
@@ -268,73 +291,58 @@ with open(dups, 'w', newline='') as CSVDupObject:
             
             for eachSPSSPhone in SPSSPhoneList:
                 for eachSIRPhone in sirPhoneList:
-                    
                     tempPhonePoints = comparePhone (eachSIRPhone, eachSPSSPhone) 
                     if tempPhonePoints > 0 :
                         matchingPhones += eachSIRPhone + ' = ' + eachSPSSPhone + ','
                         pointPhone += tempPhonePoints
  
-            if pointLastName >= 10 :
-                multFactor *= 2
-                if rowInSPSS['firstname'] == rowInSIR['fname']:
-                    multFactor *= 2
-                elif rowInSIR['fname'] in rowInSPSS['firstname']:
-                    multFactor *= 1.5
-                if pointDOB == 9:
-                    multFactor *= 1.5
-                elif pointDOB > 9:
-                    multFactor *=2
-                if pointSex >= 18:
-                    multFactor *= 2
-                if pointAddress >= 8:
-                    multFactor *= 2
-                if pointPhone >= 10:
-                    multFactor *=2
+            if pointLastName >= 6 :
+                if pointLastName >= 10:     multFactor *= 3
+                if pointFirstName >= 10:    multFactor *= 2
+                elif pointFirstName >= 6:   multFactor *= 1.5
+                if pointDOB > 9:            multFactor *= 2
+                elif pointDOB == 9:         multFactor *= 1.5
+                elif pointDOB < 0 :         multFactor /= 2
+                if pointSex >= 18:          multFactor *= 2
+                if pointAddress >= 8:       multFactor *= 2
+                if pointPhone >= 10:        multFactor *= 2
+            
                     
-            elif rowInSPSS['firstname'] == rowInSIR['fname']:
+            elif pointFirstName >= 10:
                 multFactor *=1.1
-                if rowInSIR['lname'] in rowInSPSS['lastname']:
-                    multFactor *= 1.5
-                if pointDOB == 9:
-                    multFactor *= 1.2
-                elif pointDOB > 9:
-                    multFactor *=1.5
-                if pointSex >= 18:
-                    multFactor *= 1
-                if pointAddress >= 8:
-                    multFactor *= 2
-                if pointPhone >= 10:
-                    multFactor *=2
+                if pointDOB == 9:           multFactor *= 1.2
+                elif pointDOB > 9:          multFactor *= 1.5
+                elif pointDOB < 0 :         multFactor /= 2
+                if pointSex >= 18:          multFactor *= 1.5
+                if pointAddress >= 8:       multFactor *= 2
+                if pointPhone >= 10:        multFactor *= 2
                 
 
-            elif rowInSPSS['lastname'] == rowInSIR['fname']:
-                multFactor *=1.1
-                if rowInSIR['lname'] in rowInSPSS['firstname']:
-                    multFactor *= 1.5
-                if pointDOB == 9:
-                    multFactor *= 1.2
-                elif pointDOB > 9:
-                    multFactor *=1.5
-                if pointSex >= 18:
-                    multFactor *= 1
-                if pointAddress >= 8:
-                    multFactor *= 2
-                if pointPhone >= 10:
-                    multFactor *=2
+            elif pointFirstLast >=6 and pointLastFirst >=6:
+                if pointFirstLast >=10 and pointLastFirst >=10: multFactor *= 2
+                if pointDOB == 9:       multFactor *= 1.2
+                elif pointDOB > 9:      multFactor *= 1.5
+                elif pointDOB < 0 :     multFactor /= 2
+                if pointSex >= 18:      multFactor *= 1.2
+                if pointAddress >= 8:   multFactor *= 2
+                if pointPhone >= 10:    multFactor *= 2
 
-            pointTotal = 0
-            pointTotal = pointLastName + pointFirstName + pointDOB + pointSex + pointAddress + pointPhone
+            pointTotal = (pointLastName + pointFirstName + pointFirstLast + pointLastFirst +
+                          pointDOB + pointSex + pointAddress + pointPhone)
             pointTotal *= multFactor
             pointTotal = round(pointTotal)
             if pointTotal > rowInSPSS['highPoint'] :
                 rowInSPSS['highPoint']      = pointTotal
+                rowInSPSS['dataSource']     = 'SIR'
+                rowInSPSS['multFactor']     = multFactor
                 rowInSPSS['highPhone']      = pointPhone
                 rowInSPSS['highFirstName']  = pointFirstName
                 rowInSPSS['highLastName']   = pointLastName
+                rowInSPSS['highFirstLast']  = pointFirstLast
+                rowInSPSS['highLastFirst']   = pointLastFirst
                 rowInSPSS['highAddress']    = pointAddress
                 rowInSPSS['highSex']        = pointSex
                 rowInSPSS['highDOB']        = pointDOB
-                rowInSPSS['multFactor']     = multFactor
                 
                 rowInSPSS['matchID']      = rowInSIR['id_num']
                 rowInSPSS['matchLname']   = rowInSIR['lname']
@@ -344,11 +352,106 @@ with open(dups, 'w', newline='') as CSVDupObject:
                 rowInSPSS['matchPhone']   = matchingPhones
                 rowInSPSS['matchSex']     = rowInSIR['db_sex']
 
-        a.writerow(rowInSPSS)
 
-        highPointList.append(rowInSPSS['highPoint'])
+##      Compare to Access data
+        for rowInAccess in accessDict:
+##          reset values
+            pointTotal = pointLastName = pointFirstName = pointFirstLast = pointLastFirst = 0
+            pointDOB = pointSex = pointAddress = pointPhone = 0
+            matchingPhones = ''
+            multFactor = 1
 
-        print (SPSSLine , rowInSPSS['highPoint'], rowInSPSS['firstname'], rowInSPSS['lastname'], sep=',')
+    ##      speed things ups, match only if first letter of either name match
+            includeRow = checkRow(rowInSPSS['lastname'], rowInSPSS['firstname'], rowInAccess['lname'],rowInAccess['fname'])
+            if includeRow == 0:  continue
+              
+            pointAddress = compareAddress (rowInSPSS['address1'], rowInAccess['address1'])
+
+            pointDOB = compareDOB (rowInSPSS['dob'] , rowInAccess['dob'])
+
+            pointLastName = compareName(rowInSPSS['lastname'], rowInAccess['lname'])
+
+            pointFirstName = compareName(rowInSPSS['firstname'], rowInAccess['fname'])
+
+            pointFirstLast = compareName(rowInSPSS['lastname'], rowInAccess['fname'])
+
+            pointLastFirst = compareName(rowInSPSS['firstname'], rowInAccess['lname'])
+
+            pointSex = compareSex(rowInSPSS['gendercode'], rowInAccess['sex'])
+            
+    ##      Same phone number in multiple columns is a problem.
+            accessPhoneList = []    
+            for eachAccessPhone in accessPhoneBook:
+                accessPhoneList.append(rowInAccess[eachAccessPhone])
+            accessPhoneList = sorted(set(accessPhoneList))
+            
+            for eachSPSSPhone in SPSSPhoneList:
+                for eachAccessPhone in accessPhoneList:
+                    tempPhonePoints = comparePhone (eachAccessPhone, eachSPSSPhone) 
+                    if tempPhonePoints > 0 :
+                        matchingPhones += eachAccessPhone + ' = ' + eachSPSSPhone + ','
+                        pointPhone += tempPhonePoints
+ 
+            if pointLastName >= 6 :
+                if pointLastName >= 10:     multFactor *= 3
+                if pointFirstName >= 10:    multFactor *= 2
+                elif pointFirstName >= 6:   multFactor *= 1.5
+                if pointDOB > 9:            multFactor *= 2
+                elif pointDOB == 9:         multFactor *= 1.5
+                elif pointDOB < 0 :         multFactor /= 2
+                if pointSex >= 18:          multFactor *= 2
+                if pointAddress >= 8:       multFactor *= 2
+                if pointPhone >= 10:        multFactor *= 2
+            
+                    
+            elif pointFirstName >= 10:
+                multFactor *=1.1
+                if pointDOB == 9:           multFactor *= 1.2
+                elif pointDOB > 9:          multFactor *= 1.5
+                elif pointDOB < 0 :         multFactor /= 2
+                if pointSex >= 18:          multFactor *= 1.5
+                if pointAddress >= 8:       multFactor *= 2
+                if pointPhone >= 10:        multFactor *= 2
+                
+
+            elif pointFirstLast >=6 and pointLastFirst >=6:
+                if pointFirstLast >=10 and pointLastFirst >=10: multFactor *= 2
+                if pointDOB == 9:       multFactor *= 1.2
+                elif pointDOB > 9:      multFactor *= 1.5
+                elif pointDOB < 0 :     multFactor /= 2
+                if pointSex >= 18:      multFactor *= 1.2
+                if pointAddress >= 8:   multFactor *= 2
+                if pointPhone >= 10:    multFactor *= 2
+
+            pointTotal = (pointLastName + pointFirstName + pointFirstLast + pointLastFirst +
+                          pointDOB + pointSex + pointAddress + pointPhone)
+            pointTotal *= multFactor
+            pointTotal = round(pointTotal)
+            if pointTotal > rowInSPSS['highPoint'] :
+                rowInSPSS['highPoint']      = pointTotal
+                rowInSPSS['dataSource']     = 'Access'
+                rowInSPSS['multFactor']     = multFactor
+                rowInSPSS['highPhone']      = pointPhone
+                rowInSPSS['highFirstName']  = pointFirstName
+                rowInSPSS['highLastName']   = pointLastName
+                rowInSPSS['highFirstLast']  = pointFirstLast
+                rowInSPSS['highLastFirst']   = pointLastFirst
+                rowInSPSS['highAddress']    = pointAddress
+                rowInSPSS['highSex']        = pointSex
+                rowInSPSS['highDOB']        = pointDOB
+                
+                rowInSPSS['matchID']      = rowInAccess['idnum']
+                rowInSPSS['matchLname']   = rowInAccess['lname']
+                rowInSPSS['matchFname']   = rowInAccess['fname']
+                rowInSPSS['matchDOB']     = rowInAccess['dob']
+                rowInSPSS['matchAddress'] = rowInAccess['address1']
+                rowInSPSS['matchPhone']   = matchingPhones
+                rowInSPSS['matchSex']     = rowInAccess['sex']
+
+
+##      end of specific row in SPSS comparisons, add to output and report
+
+        writeCSVrow ()
 
 CSVDupObject.close()
 
